a1b082b3ac2a016b0024c4ce34e77901ca878322
Edward Shen <edwardshen24@gmail.com>
2022-01-23 23:39:38 -0800

    worked on input field

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 const library =  document.querySelector(".library");
 const addBook = document.querySelector(".addbutton");
 
+
 let myLibrary = [];
 
-function Book(title, author, pages) {
+
+
+class Book { //class constructor 
+  constructor(title, author, pages){
   this.title = title;
   this.author = author;
   this.pages = 0;
 }
-
-const book = new Book('steve', 'james', 5);
-console.log(book.author)
-
-Book.prototype.read = function(){
-
+  isRead(){
+    return false;
+  }
 }
 
 addBook.addEventListener('click', event => {
+  //addBookToLibrary();
+  //bookForm.setAttribute("style", "display: block");
+});
+
+function addBookToLibrary(){
+  const book = new Book('steve', 'james', 5);
+  myLibrary.push(book);
+  printCard();
+  console.log(myLibrary)
+}
+
+function printCard(){
   const card = document.createElement('div');
   card.className = "card";
   library.appendChild(card);
-});
+}
+
+
+
