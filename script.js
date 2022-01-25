de0d8549b2be9b428bf3883f88f95bb0fa88c96c
Edward Shen <edwardshen24@gmail.com>
2022-01-24 23:12:19 -0800

    added dom

--- script.js
+++ script.js
@@ -28,21 +28,45 @@ submit.addEventListener('click', event => {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
-  addBookToLibrary(title, author, pages);
+  let read =  document.getElementById("readCheck")
+  let isRead;
+    if (read.checked)
+      isRead = true;
+    else  
+      isRead = false;
+  addBookToLibrary(title, author, pages,isRead);
   bookForm.setAttribute("style", "display: none");
 });
 
 
-function addBookToLibrary(title,author,pages){
-  const book = new Book(title,author,pages);
+function addBookToLibrary(title,author,pages,isRead){
+  const book = new Book(title,author,pages,isRead);
   myLibrary.push(book);
-  printCard();
+  printCard(title,author,pages,isRead);
   console.log(myLibrary)
 }
 
-function printCard(){
+function printCard(title,author,pages,isRead){
   const card = document.createElement('div');
+  const titleName = document.createElement('div');
+  const authorName = document.createElement('div');
+  const pagesAmount = document.createElement('div');
+  const checkMark = document.createElement('input');
+  checkMark.value = "read";
+  const label = document.createElement('label')
+  label.innerHTML = 'Read:';
+  checkMark.type ="checkbox";
   card.className = "card";
+  titleName.textContent = "Title: " + title;
+  authorName.textContent ="Author: "+ author;
+  pagesAmount.textContent = "Pages: "+ pages;
+  card.appendChild(titleName);
+  card.appendChild(authorName);
+  card.appendChild(pagesAmount);
+  card.appendChild(label)
+  card.appendChild(checkMark);
+  if (isRead)
+    checkMark.checked = true;
   library.appendChild(card);
 }
 
